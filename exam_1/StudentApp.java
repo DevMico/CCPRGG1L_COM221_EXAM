956ecdf2a6ef457d0e42aa794606ce9db17d0167
DevMico <60907905+DevMico@users.noreply.github.com>
2022-09-08 14:22:51 +0800

    Create StudentApp.java

---
+++ exam_1/StudentApp.java
@@ -0,0 +1,8 @@
+public class StudentApp {
+    public static void main(String[] args) {
+        Student MICOSALANDANAN = new Student("Salandanan", "Mico Ranilo", 'B', "11/14/2001", 2021300032, "salandananmb@students.national-u.edu.ph", true);
+        MICOSALANDANAN.sayMyStudentNumber(); 
+        MICOSALANDANAN.sayMyEmailAddress();
+        MICOSALANDANAN.amIAwesome();
+    }
+}
